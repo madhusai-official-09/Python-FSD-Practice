0f053a5b4d2a575ee9e74c4aeaa12c7a2a154346
madhusai-official-09 <22221a4545@gmail.com>
2026-07-14 13:30:25 +0530

    upload while_loop.py file

---
+++ while_loop.py
@@ -0,0 +1,39 @@
+# Basic Counting: Write a while loop that counts from 1 to 10 and prints each number.
+
+# num = 1
+# while num<=10:
+#     print(num)
+#     num += 1
+
+# Create a program that asks the user to enter a number, and then use a while loop to count down from that number to 1, printing each value.
+
+# num = int(input("Enter a number: "))
+# while num >=1:
+#     print(num)
+#     num -= 1
+
+# Write a while loop that calculates the sum of even numbers from 1 to 100.
+# num =  int(input("Enter a number: "))
+# while num <= 100 and num %2==0 :
+#     print(num)
+#     num += num
+
+# Create a simple number guessing game using a while loop. Generate a random number and have the user guess it, giving hints like "too high" or "too low" until they guess correctly.
+
+# import random 
+# random_number = random.randint(1,10)
+
+# while True:
+#     num = int(input("Enter a number: "))
+    
+#     if num < random_number:
+#         print("too low...")
+#     elif num > random_number:
+#         print("too high...")
+#     else:
+#         print("Your guess is correct...")
+#         break
+
+# Use a while loop to calculate the factorial of a given number.
+
+
