1f32e5119db23c7e6a6e9969ba01dcb48ce644a5
madhusai-official-09 <22221a4545@gmail.com>
2026-07-16 18:52:27 +0530

    update file

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,12 +1,54 @@
-# Write a Python program that checks if a given year is a leap year.
-# year = int(input("Enter a year:"))
-# res = "Leap Year" if (year%4==0 and year%100!=0) or (year%400==0) else "Not a leap year"
-# print(res) 
+# 1. Write a Python program that checks if a given year is a leap year.
+""" year = int(input("Enter a year:"))
+res = "Leap Year" if (year%4==0 and year%100!=0) or (year%400==0) else "Not a leap year"
+print(res) """ 
 
-# Write a Python program that checks if a given character is a vowel or a consonant.
+# 2. Write a Python program that checks if a given character is a vowel or a consonant.
 
-ch = input("Enter a character: ")
+""" ch = input("Enter a character: ")
 if ch in ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U"):
     print("The character is a vowel.")
 else:
-    print("The character is a consonant.")
+    print("The character is a consonant.") """
+    
+# 3. Write a Python program that checks if a given year is a century year (ending in '00').
+
+""" year = int(input("Enter Year: "))
+if year%100==0:
+    print("Century year ->",year)
+else:
+    print("Not a century year") """
+    
+# 4. Write a Python program that checks if a given number is prime.
+num = int(input("Enter a Number: "))
+d = 2
+while d<=num//2:
+    if num%d==0:
+        print("Not a Prime...")
+        break
+    d+=1
+else:
+    print("Prime ->",num)
+    
+# 5. Write a Python program that checks if a person is eligible to vote based on their age.
+""" age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.") """
+    
+# 6. Write a Python program that checks if a number is positive or non-positive(including zero).
+""" num = int(input("Enter a number: "))
+if num>0:
+    print("Positive")
+else:
+    print("Negative") """
+    
+# 7. Write a Python program that compares two numbers and prints the largest one.
+""" n1 = int(input("Enter 1st Number: "))
+n2 = int(input("Enter 2nd Number: "))
+if n1>n2:
+    print(n1)
+else:
+    print(n2) """
+    
--- if_statement.py
+++ if_statement.py
@@ -1,46 +1,46 @@
-# Write a Python program that checks if a number is positive.
-# num = int(input("Enter a number: "))
-# if num>0:
-#     print("Positive Number")
+# 1. Write a Python program that checks if a number is positive.
+""" num = int(input("Enter a number: "))
+if num>0:
+    print("Positive Number") """
 
-# Write a Python program that checks if a given string is empty.
-# string = input("Enter a string: ")
-# if string == (""):
-#     print("empty")
+# 2. Write a Python program that checks if a given string is empty.
+""" string = input("Enter a string: ")
+if string == (""):
+    print("empty") """
 
-# Write a Python program that determines if a number is positive, negative, or zero using only if statements and no elif or else.
+# 3. Write a Python program that determines if a number is positive, negative, or zero using only if statements and no elif or else.
 
-# num = int(input("Enter a number: "))
-# if num >0:
-#     print("Positive")
-# if num<0:
-#     print("Negative")
-# if num==0:
-#     print("Zero")
+""" num = int(input("Enter a number: "))
+if num >0:
+    print("Positive")
+if num<0:
+    print("Negative")
+if num==0:
+    print("Zero") """
 
-# Write a Python program that checks if a number is a multiple of both 3 and 5.
-# num =int(input("Enter a number: "))
-# if num%3==0 and num%5==0:
-#     print(num)
+# 4. Write a Python program that checks if a number is a multiple of both 3 and 5.
+""" num =int(input("Enter a number: "))
+if num%3==0 and num%5==0:
+    print(num) """
 
-# Write a Python program that determines if a given number is a perfect square.
-# num =int(input("Enter a number: "))
-# root = num**0.5
-# if num == root**2:
-#     print("Perfect Square: ",num)
+# 5. Write a Python program that determines if a given number is a perfect square.
+""" num =int(input("Enter a number: "))
+root = int(num**0.5)
+if root * root == num:
+    print("Perfect Square: ",num) """
 
-# Write a Python program that checks if a number is divisible by both 2 and 3.
-# num = int(input("Enter a number:"))
-# if num%2==0 and num%3==0:
-#     print(num," is divisible by both 2 and 3")
+# 6. Write a Python program that checks if a number is divisible by both 2 and 3.
+""" num = int(input("Enter a number:"))
+if num%2==0 and num%3==0:
+    print(num," is divisible by both 2 and 3") """
 
-# Write a Python program that determines if a number is a perfect cube.
-# num = int(input("Enter a number: "))
-# root = num**(1/3)
-# if num == root**3:
-#     print(num,"is a Perfect Cube")
+# 7. Write a Python program that determines if a number is a perfect cube.
+""" num = int(input("Enter a number: "))
+root = num**(1/3)
+if num == root**3:
+    print(num,"is a Perfect Cube") """
 
-# Write a Python program that checks if a given number is a multiple of 4.
-# num = int(input("Enter a number: "))
-# if num % 4 == 0:
-#     print(num,"is a multiple of 4.")
+# 8. Write a Python program that checks if a given number is a multiple of 4.
+""" num = int(input("Enter a number: "))
+if num % 4 == 0:
+    print(num,"is a multiple of 4.") """
