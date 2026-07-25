824a11e7ed223ac9fcb6bae9e45154cdebe5f013
madhusai-official-09 <22221a4545@gmail.com>
2026-07-25 19:17:54 +0530

    patterns while loop

---
+++ patterns_while_loop.py
@@ -0,0 +1,22 @@
+""" 
+            *
+           * *
+          * * *
+         * * * * """
+         
+rows = int(input("Enter number of rows: "))
+row_cnt = 1
+while row_cnt<=rows:
+    spaces = rows - row_cnt
+    
+    while spaces>0:
+        print(" ",end ="")
+        spaces-=1
+    
+    star = 1
+    while star<=row_cnt:
+        print("*",end=" ")
+        star+=1
+    print()
+        
+    row_cnt+=1
