4751e50860e23f18857fe75779a5c5f79f82b67d
madhusai-official-09 <22221a4545@gmail.com>
2026-08-26 13:33:14 +0530

    task_26.py

--- task_26.py
+++ task_26.py
@@ -59,7 +59,13 @@ print(result)  """
     new_st = {}
     for i in st:
         if i not in new_st:
-    return new_st
+            new_st[i] = 1
+        else:
+            new_st[i] += 1
+    for i in new_st:
+        if new_st[i] > 1:
+            return False
+    return True
 st = "Python"
 res = un_ch(st)
 print(res) """
@@ -72,22 +78,42 @@ print(res) """
 # Explanation:
 # Assign ranks based on the values in descending order. The key with the highest value gets rank 1, the next highest gets rank 2, and so on.
 
-""" dic = {'A': 85, 'B': 92, 'C': 78, 'D': 88}
-output= {}
-for i in dic:
-    if dic[i] > 90:
-        output[i]=1
-    elif dic[i]>85:
-        output[i] =2
-    elif dic[i]>80:
-        output[i]=3
-    else:
-        output[i]=4
-li = list(output.items())
-for i in range(0,len(li)-1):
-    for j in range(i+1,len(li)):
-        if li[i][1] > li[j][1]:
-            li[i],li[j] = li[j],li[i]
-res ={k:v for k,v in li}
-print(res) """
+""" def rank_descending_order(dic):
+    output= {}
+    for i in dic:
+        if dic[i] > 90:
+            output[i]=1
+        elif dic[i]>85:
+            output[i] =2
+        elif dic[i]>80:
+            output[i]=3
+        else:
+            output[i]=4
+    li = list(output.items())
+    for i in range(0,len(li)-1):
+        for j in range(i+1,len(li)):
+            if li[i][1] > li[j][1]:
+                li[i],li[j] = li[j],li[i]
+    res ={k:v for k,v in li}
+    return res
+dic = {'A': 85, 'B': 92, 'C': 78, 'D': 88}
+result = rank_descending_order(dic)
+print(result) """
 
+# 5. Write a Python function to check whether two lists are rotations of each other. Input: [1, 2, 3, 4, 5] [3, 4, 5, 1, 2] 
+# Output: True 
+# Explanation: Return True if one list can be obtained by rotating the other; otherwise, return False.
+
+""" def check_rotation(li1, li2):
+    if len(li1) != len(li2):
+        return False
+    for i in range(len(li1)):
+        li1.append(li1[0])
+        li1.pop(0)
+        if li1 == li2:
+            return True
+    return False    
+li1 = [1, 2, 3, 4, 5]
+li2 = [3, 4, 5, 1, 2]
+result = check_rotation(li1, li2)
+print(result) """
