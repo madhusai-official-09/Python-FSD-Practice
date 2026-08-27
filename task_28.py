8ad566e65fd7c10cbb6b6d6c609ce02a640b51c9
madhusai-official-09 <22221a4545@gmail.com>
2026-08-27 22:11:23 +0530

    task_28.py

---
+++ task_28.py
@@ -0,0 +1,67 @@
+# 1.Write a Python program to compress a string.
+# Input : aaabbc
+# Output : a3b2c1
+
+""" s = "aaabbc"
+
+def count_ch(st):
+    n_di = {}
+    n_st = ""
+    for i in st:
+        if i in n_di:
+            n_di[i] += 1
+        else:
+            n_di[i] = 1
+    key = 
+    return key
+
+ans = count_ch(s)
+print(ans) """
+
+# 2. Write a Python program to create a mirror string.
+# Input : abc
+# Output : abccba
+
+""" s = "abc"
+s2 = s[::-1]
+print(s+s2) """
+
+# 3. Write a Python program to find the longest palindromic substring.
+# Input : babad
+# Output : bab
+
+# 4.Write a Python function to remove all special characters from a string.
+# Input : a@1#b
+# Output : a1b
+
+""" st = "a@1#b"
+def spl_ch(s):
+    word = ""
+    for i in s:
+        if i.isalpha() == True or i.isnumeric():
+            word+=i
+    return word
+
+ans = spl_ch(st)
+print(ans) """
+
+# 5.Write a Python function to reverse every alternate word in a sentence.
+# Input : one two three four
+# Output : one owt three ruof
+
+""" st = "one two three four"
+def rev_alt_word(s):
+    temp = s.split()
+    word = ""
+    for i in range(len(temp)):
+        if i%2==0:
+            word += temp[i]
+            word+=" "
+        else:    
+            word += temp[i][::-1]
+            word+=" "
+    return word
+
+ans = rev_alt_word(st)
+print(ans) """
+
