3c58695e2984d7efc3c364f4a9d29768fc5da5a4
madhusai-official-09 <22221a4545@gmail.com>
2026-09-02 15:30:55 +0530

    task_34.py

--- task_33.py
+++ task_33.py
@@ -46,7 +46,7 @@ print(c(7)) """
 # 4. Write a function calculator(n) that returns three inner functions: add(x) → adds x to n sub(x) → subtracts x from n mul(x) → multiplies n by x All three functions must access the same variable n from the outer function. 
 # Input: add, sub, mul = calculator(1
 
-""" def calculator(n):
+def calculator(n):
     def addfun(x):
         nonlocal n
         n+=x
@@ -61,5 +61,5 @@ print(c(7)) """
         return n
     return addfun
 c = calculator(5)
-print(c(5)) """
+print(c(5))
             
---
+++ task_34.py
@@ -0,0 +1,4 @@
+# 1. Write a Python program using a decorator to display a message before and after executing the function. 
+# Input: hello batch 89 
+# Output: Function Started hello batch 89 Function Ended
+
