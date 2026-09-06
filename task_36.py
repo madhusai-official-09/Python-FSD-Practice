f27a14e83cb777a442dc604892250aee0f21bab6
madhusai-official-09 <22221a4545@gmail.com>
2026-09-06 10:01:34 +0530

    task_36.py

--- task_36.py
+++ task_36.py
@@ -20,7 +20,7 @@ print(res) """
 # Input: n = 48291 
 # Output: 19284
 
-def rev_num(n,rev=0):
+""" def rev_num(n,rev=0):
     if n==0:
         return  rev  
     
@@ -29,4 +29,41 @@ def rev_num(n,rev=0):
 
 n = 48291
 res = rev_num(n)
-print(res)
+print(res) """
+
+# 3. Write a recursive function to find the factorial of a given number. 
+# Input: n = 6 
+# Output: 720
+
+""" def factorial(n):
+    if n==1:
+        return 1
+    
+    return n*factorial(n-1)
+
+print(factorial(6)) """
+
+# 4. Write a recursive function to find the sum of the first n natural numbers. Input: n = 8 
+# Output: 36
+
+""" def sum_natural(n):
+    if n== 1:
+        return 1
+    
+    return n+sum_natural(n-1)
+
+print(sum_natural(8)) """
+
+# 5. Write a recursive function to find the nth Fibonacci number. 
+# Input: n = 7 
+# Output: 13
+
+""" def fibonacci(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+    
+print(fibonacci(7)) """
