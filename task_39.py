44486249a68f8c6d6379fffa871ab30a9e2f3325
madhusai-official-09 <22221a4545@gmail.com>
2026-09-10 12:53:58 +0530

    task_39.py

--- task_39.py
+++ task_39.py
@@ -18,8 +18,7 @@ s1.display_details() """
 # Input: length = 10 breadth = 5 
 # Output: Area: 50 Perimeter: 30
 
-
-class Rectangle:
+""" class Rectangle:
     def __init__(self, length, breadth):
         self.length = length
         self.breadth = breadth
@@ -31,5 +30,29 @@ class Rectangle:
         return 2 * (self.length + self.breadth)
 
 r1 = Rectangle(10, 5)
-print(f"Area: {r1.calculate_area()} Perimeter: {r1.calculate_perimeter()}")
+print(f"Area: {r1.calculate_area()} Perimeter: {r1.calculate_perimeter()}") """
 
+# 3. Create a class Employee with attributes name, employee_id, and salary. Create an object and display the employee details. Also calculate the salary after adding a ₹5000 bonus. 
+# Input: name = "Arjun" employee_id = 101 salary = 30000 
+# Output: Name: Arjun Employee ID: 101 Salary: 30000 Updated Salary: 35000
+
+class Employee:
+    def __init__(self,name,emply_id,salary):
+        self.name = name
+        self.emply_id=emply_id
+        self.salary = salary
+    def bonus(self):
+        org_salary = self.salary+5000
+        return org_salary
+    def display(self):
+        print(self.name)
+        print(self.emply_id)
+        print(self.bonus())
+        
+employee1=Employee("Arjun",101,30000)
+employee1.bonus()
+employee1.display()
+
+# 4. Create a class Mobile with attributes brand, model, and price. Create an object and display the mobile details. 
+# Input: brand = "Samsung" model = "A55" price = 30000 
+# Output: Brand: Samsung Model: A55 Price: 30000
